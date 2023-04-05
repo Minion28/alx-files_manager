e8c5c3c2824b6054e75a205f4111a8ae9ca560cc
Kelvin Mwanzia <wambuakelvin14@gmail.com>
2023-04-05 09:57:13 +0300

    redis utils

---
+++ utils/db.js
@@ -0,0 +1,40 @@
+import { MongoClient } from 'mongodb';
+
+const Host = process.env.DB_HOST || 'localhost';
+const port = process.env.DB_PORT || 27017;
+const Database = process.env.DB_DATABASE || 'files_manager';
+const url = `mongodb://${Host}:${port}`;
+
+class DBClient {
+  constructor() {
+    MongoClient.connect(url, { useUnifiedTopology: true }, (err, client) => {
+      if (!err) {
+        this.db = this.client.db(Database);
+        this.usersCollection = this.db.collection('users');
+        this.filesCollection = this.db.collection('files');
+      } else {
+        console.log(err.message);
+        this.db = false;
+      }
+    });
+  }
+
+  isAlive() {
+    return Boolean(this.db);
+  }
+
+
+  async nbUsers() {
+    const numberOfUsers = this.usersCollection.countDocuments();
+    return numberOfUsers;
+  }
+
+  async nbFiles() {
+    const numberOfFiles = this.filesCollection.countDocuments();
+    return numberOfFiles;
+  }
+}
+
+const dbClient = new DBClient();
+
+export default dbClient;
